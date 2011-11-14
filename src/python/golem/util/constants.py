40f0aff38004d8284dca83106280096a103697b5
thomasr <thomasr@a6e02f90-e1d0-4296-904b-e314d7e4afaa>
2011-11-14 16:03:22 +0000

    Fixed incompatibility with 'FactArg' in Form 4.0.
    Added Copyright notice everywhere.
    
    
    
    git-svn-id: svn+ssh://svn.hepforge.org/hepforge/svn/gosam/trunk@5 a6e02f90-e1d0-4296-904b-e314d7e4afaa

--- src/python/golem/util/constants.py
+++ src/python/golem/util/constants.py
@@ -26,6 +26,31 @@ AUTHORS = {
       "Gionata Luisoni": ["gionata.luisoni@durham.ac.uk"]
 }
 
+LICENSE = [
+"",
+"",
+"    This program is free software: you can redistribute it and/or modify",
+"    it under the terms of the GNU General Public License as published by",
+"    the Free Software Foundation, either version 3 of the License, or",
+"    (at your option) any later version.",
+"",
+"    This program is distributed in the hope that it will be useful,",
+"    but WITHOUT ANY WARRANTY; without even the implied warranty of",
+"    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the",
+"    GNU General Public License for more details.",
+"",
+"    You should have received a copy of the GNU General Public License",
+"    along with this program.  If not, see <http://www.gnu.org/licenses/>.",
+"",
+"    Scientific publications prepared using the present version of",
+"    GoSam or any modified version of it or any code linking to",
+"    GoSam or parts of it should make a clear reference to the publication:",
+"",
+"        G. Cullen et al.,",
+"        ``Automated One-Loop Calculations with GoSam,''",
+"        arXiv:1111.2034 [hep-ph]"
+]
+
 SCATTER_ASCIIART = [
    r'             \         . . . .   ',
    r'              \       . . .      ',
