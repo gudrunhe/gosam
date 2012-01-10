4bbc65027097b1f9d6817de49a8b3f362422fa83
Thomas Reiter <reiterth@mpp.mpg.de>
2012-01-10 14:34:29 +0000

    Replacing trunk completely with files from branches/unstable    tree conflicts made me sick
    git-svn-id: svn+ssh://svn.hepforge.org/hepforge/svn/gosam/trunk@45 a6e02f90-e1d0-4296-904b-e314d7e4afaa

--- src/python/golem/installation.py
+++
@@ -1,1 +0,0 @@
-# This is a dummy file. Do not remove.
--- templates/common/precision_pjfry.f90
+++
@@ -1,8 +0,0 @@
-[% ' vim: ts=3:sw=3:expandtab:syntax=golem
-%]module     [% process_name asprefix=\_ %]precision_pjfry
-   implicit none
-   private
-
-   integer, parameter, public :: ki_pjf = kind(1.0d0)
-
-end module [% process_name asprefix=\_ %]precision_pjfry
