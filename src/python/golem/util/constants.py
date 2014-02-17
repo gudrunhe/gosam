536ea7140271c7c764cd1e34f1a21e34cf6b5f46
jfsoden <jfsoden@a6e02f90-e1d0-4296-904b-e314d7e4afaa>
2014-02-17 10:39:22 +0000

    Update years in copyright lines.
    git-svn-id: svn+ssh://svn.hepforge.org/hepforge/svn/gosam/branches/gionny-develop@474 a6e02f90-e1d0-4296-904b-e314d7e4afaa

--- src/python/golem/util/constants.py
+++ src/python/golem/util/constants.py
@@ -89,7 +89,7 @@ CLINES = [
               ( ".".join(map(str,golem.installation.GOLEM_VERSION)),
                 golem.installation.GOLEM_REVISION),
             '',
-            '(c) The GoSam Collaboration 2011-2013',
+            '(c) The GoSam Collaboration 2011-2014',
             ''
       ]
 
