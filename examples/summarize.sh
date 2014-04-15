1b616ed7c5bda843c06efdd50f65d3a6b7484a1e
jfsoden <jfsoden@a6e02f90-e1d0-4296-904b-e314d7e4afaa>
2014-04-15 13:43:47 +0000

    Support "-" in examples names in examples/summarize.sh
    git-svn-id: svn+ssh://svn.hepforge.org/hepforge/svn/gosam/branches/gionny-develop@597 a6e02f90-e1d0-4296-904b-e314d7e4afaa

--- examples/summarize.sh
+++ examples/summarize.sh
@@ -4,12 +4,12 @@ printf "\n"
 printf "\033[0;32m"
 find -type d -name 'test' -exec grep -Hl "@@@ SUCCESS @@@" \{}/test.log \
 	   2>/dev/null \; | \
-	sed 's/\.\/\([A-Za-z0-9_]*\)\/test\/test.log/+ \1 (succeeded)/' | \
+	sed 's/\.\/\([A-Za-z0-9_-]*\)\/test\/test.log/+ \1 (succeeded)/' | \
 	sort
 printf "\033[0m"
 printf "\033[1;31m"
 find -type d -name 'test' -exec grep -HL "@@@ SUCCESS @@@" \{}/test.log \; | \
-	sed 's/\.\/\([A-Za-z0-9_]*\)\/test\/test.log/+ \1 (FAILED)/' | \
+	sed 's/\.\/\([A-Za-z0-9_-]*\)\/test\/test.log/+ \1 (FAILED)/' | \
 	sort
 printf "\033[0m"
 printf "\n"
