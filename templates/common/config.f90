6132625888f1db2e44eebe4384c860fd0e9f6e91
Johann Felix von Soden-Fraunhofen <jfsoden@mpp.mpg.de>
2014-04-29 13:03:09 +0000

    Switch off samurai_group_numerators temporary.
    git-svn-id: svn+ssh://svn.hepforge.org/hepforge/svn/gosam/branches/gosam-2.0@674 a6e02f90-e1d0-4296-904b-e314d7e4afaa

--- templates/common/config.f90
+++ templates/common/config.f90
@@ -103,7 +103,7 @@
    ! calls. Unless you really know what you do, you should stick to the
    ! default value.
    integer :: samurai_istop = 0
-   logical :: samurai_group_numerators = .true.[%
+   logical :: samurai_group_numerators = .false.[%
       @end @if extension samurai %]
 
    [% @if extension ninja %]
