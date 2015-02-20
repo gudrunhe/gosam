94b8b5d195425f70c41cd1479ea3620301c0d13f
Johann Felix von Soden-Fraunhofen <jfsoden@mpp.mpg.de>
2015-02-20 15:00:48 +0000

    BLHA2: Add initialization of samurai if first subprocess is not virtual.
    git-svn-id: svn+ssh://svn.hepforge.org/hepforge/svn/gosam/branches/jfsoden-develop@754 a6e02f90-e1d0-4296-904b-e314d7e4afaa

--- src/python/golem/util/olp.py
+++ src/python/golem/util/olp.py
@@ -610,6 +610,15 @@ def process_order_file(order_file_name, f_contract, path, default_conf,
       # store initial symmetries infos
       start_symmetries = conf["symmetries"]
 
+      # handle case that first subprocess does not initalize samurai (LO process)
+      for subprocess in subprocesses.values():
+         sp_conf = subprocess.getConf(subprocesses_conf[int(subprocess)], path)
+         if "samurai" in sp_conf["reduction_programs"] and sp_conf["olp.no_loop_level"]=="False":
+            # add samurai to first subprocess, which is called by "initgolem(true)"
+            first_subprocess = int(subprocesses.values()[0])
+            subprocesses_conf[first_subprocess]["initialization-auto.extensions"]="samurai"
+            break
+
       for subprocess in subprocesses.values():
          process_path = subprocess.getPath(path)
          subprocess_conf = subprocess.getConf(subprocesses_conf[int(subprocess)], path)
