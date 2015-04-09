384a04f3df6523ca50d37564005d67b83bdaa11b
Nicolas Greiner <greiner@mpp.mpg.de>
2015-04-09 12:15:34 +0000

    change if statement
    git-svn-id: svn+ssh://svn.hepforge.org/hepforge/svn/gosam/branches/nico-develop@789 a6e02f90-e1d0-4296-904b-e314d7e4afaa

--- src/python/golem/util/main_misc.py
+++ src/python/golem/util/main_misc.py
@@ -160,8 +160,8 @@ def generate_process_files(conf, from_scratch=False):
 
 	if flag_create_ff_files:
 		create_ff_files(conf, in_particles, out_particles)
-		
-	if conf["__REDUZE__"]:
+	
+	if conf["__REDUZE__"]==True:   
 	    copy_file(os.path.join(os.getcwd(),conf.getProperty("projectors")), os.path.join(conf.getProperty("process_path"),'codegen','projectors.hh'))
 	    copy_file(os.path.join(os.getcwd(),conf.getProperty("integral_families")), os.path.join(conf.getProperty("process_path"),'codegen','reduze','config','integral_families.yaml'))
 
