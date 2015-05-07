0da429a6dcf70271d4961ec33580d52c555a3015
Stephen Jones <sjones@mpp.mpg.de>
2015-05-07 11:40:55 +0000

    Fix bug in naming of incoming/outgoing momenta in secdec.m
    git-svn-id: svn+ssh://svn.hepforge.org/hepforge/svn/gosam/branches/nico-develop@822 a6e02f90-e1d0-4296-904b-e314d7e4afaa

--- templates/amplitude/secdec.m
+++ templates/amplitude/secdec.m
@@ -2,10 +2,10 @@ Dim=4-2*eps;
 prefactor=Gamma[1+eps]^2;
 
 momlist=ToExpression[Table[StringJoin["p", ToString[i]], {i, 1, [% loop %]}]];
-gosam_incoming = {[% @for particles initial %]k[%index%][% @if eval index .lt. num_in %], [% @end @if %][%@end @for %]};
-gosam_outgoing = {[% @for particles final %]k[%index%][% @if eval index .lt. num_out %], [% @end @if %][%@end @for %]};
+GosamIncoming = {[% @for particles initial %]k[%index%][% @if eval index .lt. num_in %], [% @end @if %][%@end @for %]};
+GosamOutgoing = {[% @for particles final %]k[%index%][% @if eval index .lt. num_out %], [% @end @if %][%@end @for %]};
 
-ExternalMomenta=Join[gosam_incoming,gosam_outgoing];
+ExternalMomenta=Join[GosamIncoming,GosamOutgoing];
 KinematicInvariants={[% @for mandelstam non-zero non-mass %][% @if eval .not. is_first %],[% @end @if %][% symbol %][% @end @for %]};
 Masses={[% @for all_masses %][% @if eval .not. is_first %],[% @end @if %][% mass %][% @end @for %]};
 ScalarProductRules = {[%@for mandelstam_subs diagonal upper %]
