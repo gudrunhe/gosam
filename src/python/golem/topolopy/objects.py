b8545f0869856deb03f51b71487f0e67583b405f
Johann Felix von Soden-Fraunhofen <jfsoden@mpp.mpg.de>
2015-05-22 16:23:40 +0000

    Bugfix in python-filter: Find particles properly with UFO-models.
    git-svn-id: svn+ssh://svn.hepforge.org/hepforge/svn/gosam/branches/jfsoden-develop@834 a6e02f90-e1d0-4296-904b-e314d7e4afaa

--- src/python/golem/topolopy/objects.py
+++ src/python/golem/topolopy/objects.py
@@ -805,14 +805,16 @@ class DiagramComponent:
       if len(rays) == 0 and len(query) == 0:
          return True
       r1 = rays[0]
+      r1_unquoted = r1
       rem = rays[1:]
       if r1[0:4] == 'part':
+         r1_unquoted = str(r1)
          r1='\'%s\'' % r1
       for i, q in enumerate(query):
          if q is None:
             if self.match_fields(rem, query[:i] + query[i+1:]):
                return True
-         elif r1 in q:
+         elif (r1 in q) or (r1_unquoted in q):
             if self.match_fields(rem, query[:i] + query[i+1:]):
                return True
       return False
