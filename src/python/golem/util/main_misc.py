b882264fa79d633c2bcc896d883dca1ed28471bb
Stephan Jahn <sjahn@mpp.mpg.de>
2015-07-13 18:12:55 +0000

    raise error if helsum is used for loop-induced process
    o the current implementation only works if born-level present
    
    git-svn-id: svn+ssh://svn.hepforge.org/hepforge/svn/gosam/branches/sjahn-develop@882 a6e02f90-e1d0-4296-904b-e314d7e4afaa

--- src/python/golem/util/main_misc.py
+++ src/python/golem/util/main_misc.py
@@ -628,6 +628,10 @@ def workflow(conf):
 		conf["shared.ldflags"]="-fPIC"
 
 	if conf["helsum"]:
+		if not conf.getBooleanProperty("generate_lo_diagrams"):
+			raise GolemConfigError(
+				'The "helsum" feature is not implemented for loop-induced processes.\n' +
+				'Set "helsum=false" for the selected process.\n')
 		if "generate-all-helicities" not in ext:
 			ext.append("generate-all-helicities")
 		if conf.getProperty("polvec")=="numerical" or "numpolvec" in ext:
@@ -635,6 +639,10 @@ def workflow(conf):
 				'The "helsum" feature is only implemented for explicit\n' +
 				'polarization vectors. Please either set "helsum=false"\n' +
 				'or "polvec=explicit" in the input card."\n')
+		if "autotools" in ext:
+			raise GolemConfigError(
+				'The "helsum" feature is not implemented for autotools.\n' +
+				'Either set "helsum=false" or remove "autotools" from "extensions".\n')
 	else:
 		if conf.getProperty("polvec")=="numerical" and not "numpolvec" in ext:
 			ext.append("numpolvec")
