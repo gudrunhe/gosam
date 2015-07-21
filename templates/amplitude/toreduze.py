1f0f9e2574e19ee77a8898b8d0a93e45c277cfce
Stephen Jones <sjones@mpp.mpg.de>
2015-07-21 14:22:32 +0000

    Adjust file names, template.xml
    git-svn-id: svn+ssh://svn.hepforge.org/hepforge/svn/gosam/branches/nico-develop@890 a6e02f90-e1d0-4296-904b-e314d7e4afaa

--- templates/amplitude/toreduze.py
+++ templates/amplitude/toreduze.py
@@ -14,11 +14,11 @@ def main(argv):
    try:
       opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
    except getopt.GetoptError:
-      print 'test.py -i <inputfile> -o <outputfile>'
+      print 'toreduze.py -i <inputfile> -o <outputfile>'
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print 'test.py -i <inputfile> -o <outputfile>'
+         print 'toreduze.py -i <inputfile> -o <outputfile>'
          sys.exit()
       elif opt in ("-i", "--ifile"):
          inputfile = arg
