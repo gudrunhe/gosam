a4c7c2c1622641f7972047402b6a9521aad01c6f
Nicolas Greiner <greiner@nazgul.physik.uzh.ch>
2016-10-04 11:04:24 +0200

    added mass- and yukawa ct for 2loop

--- templates/codegen/symbols.hh
+++ templates/codegen/symbols.hh
@@ -41,9 +41,9 @@ Vector ZERO, vDUMMYA;
 ***** WARNING THE FOLLOWING CODE DOES NOT WORK IN GENERAL *****
 #IfDef `MASSCT'
 * used for mass CT insertions --- ERROR dZmp SHOULD NOT BE LIMITED TO 1 HERE!
-Symbols dZmp(0:`MASSCT'); * Symbol to count number of mass CT insertions
+Symbols dZmp(0:`MASSCT'),dZmp2(0:`MASSCT'); * Symbol to count number of mass CT insertions
 CFunctions deltaZm; * Function to store the loop order and mass of the mass CT insertion
-CFunctions proplorentzct;
+CFunctions proplorentzct,proplorentzct1;
 #EndIf
 ***** END WARNING *****
         
