e6e127c6ec3777c800362ba50b445da42a5e9479
Stephen Jones <sjones@mpp.mpg.de>
2016-11-04 21:59:26 +0100

    [coefficients] generate list of invariants in cpp format with template system

---
+++ templates/amplitude/invariants.hpp
@@ -0,0 +1,20 @@
+/*
+ *
+ * Defines the complete list of invariants known to GoSam
+ * Written by: template.xml
+ *
+ */
+
+#ifndef invariants_hpp_included
+#define invariants_hpp_included
+
+// TODO include header with typedef for invariants_t and invariant_t
+
+struct invariants_t
+{
+[% @for mandelstam sym_prefix=es non-zero non-mass%]
+    invariant_t [%symbol%];[% @end @for mandelstam non-zero%]
+
+};
+
+#endif
