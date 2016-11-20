372e6f16a030956a291e56297925dfb4466bd842
Stephen Jones <sjones@mpp.mpg.de>
2016-11-20 18:44:48 +0100

    [coefficients] make invariants constant in coefficient functions

--- templates/amplitude/coefficients/loop/src/invariants_hunk.cpp
+++ templates/amplitude/coefficients/loop/src/invariants_hunk.cpp
@@ -1,2 +1,2 @@
 [% @for mandelstam sym_prefix=es non-zero non-mass%]
-invariant_t [%symbol%]=invariants.[%symbol%];[% @end @for mandelstam non-zero%]
+const invariant_t [%symbol%]=invariants.[%symbol%];[% @end @for mandelstam non-zero%]
