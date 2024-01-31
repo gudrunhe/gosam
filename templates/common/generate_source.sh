321fc94a5d836cbceb1c98bd076b139a6fca8c66
Jens Braun <jens.braun@student.kit.edu>
2024-01-31 22:04:36 +0100

    Adapted meson build files to work for NLO processes and added previously missing build file

--- templates/common/generate_source.sh
+++ templates/common/generate_source.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd "${CURRENT_SOURCE_DIR}"
-make -f Makefile.source source
+make -f Makefile.source > /dev/null
 cp -t "${OUTDIR}" model.f90 color.f90 version.f90
--- templates/helicity/generate_source.sh
+++ templates/helicity/generate_source.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
 cd "${CURRENT_SOURCE_DIR}"
-make -f Makefile.source all_source
-cp -t "${OUTDIR}" diagramsl0.f90
+[% @if diagsum %]
+if [ -e ../diagrams-1.hh ] && head -n 100 ../diagrams-1.hh | grep -q 'Local' ;
+	then
+		sed -i -e "s/Local diagram/Id diag/g" ../diagrams-1.hh;
+fi[%
+@end @if %]
+if [ -f Makefile.source ];
+	then
+		make -f Makefile.source > /dev/null;
+fi
+cp -t "${OUTDIR}" [% @if generate_lo_diagrams %]diagramsl0*.f90 [% @end @if generate_lo_diagrams %] [% @if generate_nlo_virt %]d*h*.f90 abbrev*h*.f90[% @end @if generate_nlo_virt %]
