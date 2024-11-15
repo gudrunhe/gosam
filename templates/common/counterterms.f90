36841fea2a9b1f4ee613cee41c7a9a5a9397e9c5
MariusH <marius.hoefer@kit.edu>
2024-11-15 15:06:09 +0100

    Renaming Yukawa and mass counterterms

--- templates/common/counterterms.f90
+++ templates/common/counterterms.f90
@@ -1,99 +1,113 @@
 [%' vim: sw=3:syntax=golem
 '%]module     [% process_name asprefix=\_ %]counterterms
-   use [% @if internal OLP_MODE %][% @else %][% process_name%]_[% @end @if %]config, only: ki, &
-    & renormalisation, renorm_logs, renorm_yukawa, renorm_mqse   
+   use [% @if internal OLP_MODE %][% @else %][% process_name%]_[% @end @if %]config, only: ki
    implicit none
    save
 
    private
 
-public :: yukctOS, yukctMSbar, massctOS
+public :: counterterm_yukawa_OS, counterterm_yukawa_MSbar, counterterm_mass_OS 
 
 contains
-
-!---#[ function yukctOS:
-   function yukctOS(renorm,eps,scale2,m)
+ 
+!---#[ function counterterm_yukawa_OS:
+   function counterterm_yukawa_OS(renorm,eps,scale2,m) result(ct)
       use [% process_name asprefix=\_ %]color, only: CF
+      use [% @if internal OLP_MODE %][% @else %][% process_name%]_[% @end @if %]config, only: &
+         & renormalisation, renorm_logs, renorm_yukawa
       implicit none
-      real(ki) :: yukctOS, scale2, m
+      real(ki) :: ct, scale2, m
       integer :: eps
       logical :: renorm
 
+      ct = 0.0_ki   
+
       if (.not.(renorm.and.renorm_yukawa)) then
-         yukctOS = 0.0_ki
          return
       end if
 
       select case (eps)
+         case(-2)
+            ct = 0.0_ki
          case(-1)
-            yukctOS = -1.5_ki*CF
+            ct = -1.5_ki*CF
          case(0)
-            yukctOS = -[% @if extension dred %]2.5[% @else %]2.0[% @end @if %]_ki*CF
+            ct = -[% @if extension dred %]2.5[% @else %]2.0[% @end @if %]_ki*CF
             if (renorm_logs) then
-               yukctOS = yukctOS - 1.5_ki*CF*reglog(scale2/m/m)
+               ct = ct - 1.5_ki*CF*reglog(scale2/m/m)
             end if
          case default
-            print *, "ERROR: In function yukctOS: unkown epsilon power."
+            print *, "ERROR: In function counterterm_yukawa_OS: unkown epsilon power."
             stop
       end select
 
-   end function     yukctOS
-!---#] function yukctOS:
-!---#[ function yukctMSbar:
-   function yukctMSbar(renorm,eps)
+   end function counterterm_yukawa_OS
+!---#] function counterterm_yukawa_OS:
+!---#[ function counterterm_yukawa_MSbar:
+   function counterterm_yukawa_MSbar(renorm,eps) result(ct)
       use [% process_name asprefix=\_ %]color, only: CF
+      use [% @if internal OLP_MODE %][% @else %][% process_name%]_[% @end @if %]config, only: &
+         & renormalisation, renorm_logs, renorm_yukawa
       implicit none
-      real(ki) :: yukctMSbar
+      real(ki) :: ct
       integer :: eps
       logical :: renorm
 
+      ct = 0.0_ki
+
       if (.not.(renorm.and.renorm_yukawa)) then
-         yukctMSbar = 0.0_ki
          return
       end if
 
       select case (eps)
+         case(-2)
+            ct = 0.0_ki
          case(-1)
-            yukctMSbar = -1.5_ki*CF
+            ct = -1.5_ki*CF
          case(0)
-            yukctMSbar = -[% @if extension dred %]0.5[% @else %]0.0[% @end @if %]_ki*CF
+            ct = -[% @if extension dred %]0.5[% @else %]0.0[% @end @if %]_ki*CF
          case default
-            print *, "ERROR: In function yukctMSbar: unkown epsilon power."
+            print *, "ERROR: In function counterterm_yukawa_MSbar: unkown epsilon power."
             stop
       end select
 
-   end function yukctMSbar
-!---#] function yukctMSbar:
-!---#[ function massctOS:
-   function massctOS(renorm,eps,scale2,m)
+   end function counterterm_yukawa_MSbar
+!---#] function counterterm_yukawa_MSbar:
+!---#[ function counterterm_mass_OS:
+   function counterterm_mass_OS(renorm,eps,scale2,m) result(ct)
       use [% process_name asprefix=\_ %]color, only: CF
+      use [% @if internal OLP_MODE %][% @else %][% process_name%]_[% @end @if %]config, only: &
+         & renormalisation, renorm_logs, renorm_mqse
       implicit none
-      real(ki) :: massctOS, scale2, m
+      real(ki) :: ct, scale2, m
       integer :: eps
       logical :: renorm
+
+      ct = 0.0_ki
 
       if (.not.(renorm.and.renorm_mqse)) then
-         massctOS = 0.0_ki
          return
       end if
 
-      select case (eps)
+      select case (eps)  
+         case(-2)
+            ct = 0.0_ki 
          case(-1)
-            massctOS = -1.5_ki*CF
+            ct = -1.5_ki*CF
          case(0)
-            massctOS = -[% @if extension dred %]2.5[% @else %]2.0[% @end @if %]_ki*CF
+            ct = -[% @if extension dred %]2.5[% @else %]2.0[% @end @if %]_ki*CF
             if (renorm_logs) then
-               massctOS = (massctOS - 1.5_ki*CF*reglog(scale2/m/m))
+               ct = ct - 1.5_ki*CF*reglog(scale2/m/m)
             end if
          case default
-            print *, "ERROR: In function massctOS: unkown epsilon power."
+            print *, "ERROR: In function counterterm_mass_OS: unkown epsilon power."
             stop
       end select
 
-      massctOS = m*massctOS
+      ct = m*ct
 
-   end function massctOS
-!---#] function massctOS:
+   end function counterterm_mass_OS
+!---#] function counterterm_mass_OS:
 !---#[ function reglog:
    function reglog(r)
       implicit none
