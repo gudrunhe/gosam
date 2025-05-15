69d222865e5f9a5e342cd2de96946443c046be6b
Stephen Jones <stephen.jones@durham.ac.uk>
2025-05-15 19:15:39 +0100

    fixup! [rescue] rework rescue system

--- templates/matrix/matrix.f90
+++ templates/matrix/matrix.f90
@@ -369,13 +369,14 @@ contains
       if(amp(2) .ne. 0.0_ki .and. amp(3) .ne. 0.0_ki) then
         spprec1 = -int(log10(abs((amp(3)/amp(2)))))
       endif
+      icheck=1                                                              ! ACCEPT
       if(spprec1.lt.PSP_chk_li1.and.spprec1.ge.PSP_chk_li2) then
          icheck=2                                                           ! ROTATION
-      end if
+      endif
       if(spprec1.lt.PSP_chk_li2) then                                       ! RESCUE
          icheck=3
          fpprec1=-10        ! Set -10 as finite part precision
-      end if[%
+      endif[%
 @else %]
       ! poles should match IR subtraction prediction
       ! write(*,*) '--performing pole_check (ir_subtraction)--' 
@@ -388,8 +389,11 @@ contains
       else
          kfac = 0.0_ki
       endif
+      icheck=1                                                              ! ACCEPT
       if(spprec1.lt.PSP_chk_th1.and.spprec1.ge.PSP_chk_th2 &
-           .or.(kfac.gt.PSP_chk_kfactor.and.PSP_chk_kfactor.gt.0)) icheck=2 ! ROTATION
+           .or.(kfac.gt.PSP_chk_kfactor.and.PSP_chk_kfactor.gt.0)) then
+         icheck=2                                                           ! ROTATION
+      endif
       if(spprec1.lt.PSP_chk_th2) then                                       ! RESCUE
          icheck=3
          fpprec1=-10        ! Set -10 as finite part precision
@@ -420,9 +424,6 @@ contains
       amprot=0.0_ki
       angle = 1.234_ki
       fpprec1 = 18
-      !****************************************
-      !   Step 1 - check double vs double_rot
-      !****************************************
       ! write(*,*) '--double/double rot test---' 
       do irot = 1,[%num_legs%]
          vecsrot(irot,1) = vecs(irot,1)
@@ -438,14 +439,14 @@ contains
       endif[%
       @if anymember PoleRotation PSP_chk_method ignore_case=true %]
       icheck=1                                                       ! ACCEPTED
-      if(fpprec1.lt.PSP_chk_th3) icheck=3                            ! RESCUE[%
+      if(fpprec1.lt.PSP_chk_th3) icheck=2                            ! RESCUE[%
       @else %]
       icheck=1                                                       ! ACCEPTED
-      if(fpprec1.lt.PSP_chk_li3) icheck=3                            ! RESCUE[%
+      if(fpprec1.lt.PSP_chk_li3) icheck=2                            ! RESCUE[%
       @end @if %]
       prec = fpprec1
       ! if(icheck.eq.1) write(*,*) 'passed double vs double_rot', amp(2), amprot(2), fpprec1, prec 
-      ! if(icheck.eq.3) write(*,*) 'failed double vs double_rot', amp(2), amprot(2), fpprec1, prec 
+      ! if(icheck.eq.2) write(*,*) 'failed double vs double_rot', amp(2), amprot(2), fpprec1, prec 
    end subroutine rotation_check
    !---#] subroutine rotation_check[%
    @if extension quadruple %]
@@ -461,7 +462,7 @@ contains
       real(ki), intent(in) :: scale2
       real(ki), dimension(1:4), intent(inout) :: amp
       real(ki), dimension(1:4), intent(out) :: ampdef
-      real(ki), dimension(1:4), intent(in) :: amprot
+      real(ki), dimension(1:4), intent(inout) :: amprot
       real(ki), dimension(1:4), intent(out) :: ampres
       real(ki), dimension(1:4), intent(out) :: ampresrot
       real(ki_qp), dimension(1:4) :: amp_qp, amprot_qp
@@ -477,41 +478,56 @@ contains
       ampres=0.0_ki
       ampresrot=0.0_ki
       angle = 1.234_ki
+      fpprec1 = 18
+      fpprec2 = 18
+
+      !****************************************
+      !   Step 1 - check double vs double_rot
+      !****************************************
+      if(icheck.eq.3) then
+         ! sets amprot, prec, icheck
+         call rotation_check(vecs, scale2, amp, amprot, prec, icheck, ok, h)
+      endif
+      if(icheck.eq.2) icheck=3 ! double/double_rot has failed
+
       tmp_red_int = reduction_interoperation
       reduction_interoperation = reduction_interoperation_rescue
-      !******************************************************************
-      !   Step 2 - double/double_rot do not agree to PSP_chk_th/li5 digits
-      !            compute quad
-      !******************************************************************
-      ! write(*,*) '--double does not agree with double_rot, computing quad--', amp(2), amprot(2) 
-      scale2_qp = real(scale2,ki_qp)
-      vecs_qp = vecs
-      call adjust_kinematics_qp(vecs_qp)
-      call samplitudel01_qp(vecs_qp, scale2_qp, amp_qp, rat2_qp, ok, h)
-      ampres = real(amp_qp,ki)
-      amp=ampres ! update amplitude
 
       !******************************************************************
-      !   Step 2a - check double vs quad, double_rot vs quad
+      !   Step 2 - double/double_rot do not agree to PSP_chk_th/li3 digits
+      !            compute quad
       !******************************************************************
-      if((ampdef(2)-amp(2)) .ne. 0.0_ki) then
-         fpprec1 = -int(log10(abs((ampdef(2)-amp(2))/((ampdef(2)+amp(2))/2.0_ki))))
+      if(icheck.eq.3) then
+         ! write(*,*) '--double does not agree with double_rot, computing quad--', amp(2), amprot(2) 
+         scale2_qp = real(scale2,ki_qp)
+         vecs_qp = vecs
+         call adjust_kinematics_qp(vecs_qp)
+         call samplitudel01_qp(vecs_qp, scale2_qp, amp_qp, rat2_qp, ok, h)
+         ampres = real(amp_qp,ki)
+         amp=ampres ! update amplitude
+
+         !******************************************************************
+         !   Step 2a - check double vs quad, double_rot vs quad
+         !******************************************************************
+         if((ampdef(2)-amp(2)) .ne. 0.0_ki) then
+            fpprec1 = -int(log10(abs((ampdef(2)-amp(2))/((ampdef(2)+amp(2))/2.0_ki))))
+         endif
+         if((amprot(2)-amp(2)) .ne. 0.0_ki) then
+            fpprec2 = -int(log10(abs((amprot(2)-amp(2))/((amprot(2)+amp(2))/2.0_ki))))
+         endif[%
+         @if anymember PoleRotation PSP_chk_method ignore_case=true %]
+         icheck=1                                                       ! ACCEPTED
+         if(fpprec1.lt.PSP_chk_th4) icheck=3                            ! RESCUE
+         if(fpprec2.lt.PSP_chk_th4) icheck=3                            ! RESCUE[%
+         @else %]
+         icheck=1                                                       ! ACCEPTED
+         if(fpprec1.lt.PSP_chk_li4) icheck=3                            ! RESCUE
+         if(fpprec2.lt.PSP_chk_li4) icheck=3                            ! RESCUE[%
+         @end @if %]
+         prec = min(fpprec1,fpprec2)
+         ! if(icheck.eq.1) write(*,*) 'passed double/double_rot vs quad', amp(2), amprot(2), amp_qp(2), fpprec1, fpprec2, prec 
+         ! if(icheck.eq.3) write(*,*) 'failed double/double_rot vs quad', amp(2), amprot(2), amp_qp(2), fpprec1, fpprec2, prec
       endif
-      if((amprot(2)-amp(2)) .ne. 0.0_ki) then
-         fpprec2 = -int(log10(abs((amprot(2)-amp(2))/((amprot(2)+amp(2))/2.0_ki))))
-      endif[%
-      @if anymember PoleRotation PSP_chk_method ignore_case=true %]
-      icheck=1                                                       ! ACCEPTED
-      if(fpprec1.lt.PSP_chk_th4) icheck=3                            ! RESCUE
-      if(fpprec2.lt.PSP_chk_th4) icheck=3                            ! RESCUE[%
-      @else %]
-      icheck=1                                                       ! ACCEPTED
-      if(fpprec1.lt.PSP_chk_li4) icheck=3                            ! RESCUE
-      if(fpprec2.lt.PSP_chk_li4) icheck=3                            ! RESCUE[%
-      @end @if %]
-      prec = min(fpprec1,fpprec2)
-      ! if(icheck.eq.1) write(*,*) 'passed double/double_rot vs quad', amp(2), amprot(2), amp_qp(2), fpprec1, fpprec2, prec 
-      ! if(icheck.eq.3) write(*,*) 'failed double/double_rot vs quad', amp(2), amprot(2), amp_qp(2), fpprec1, fpprec2, prec 
 
       !******************************************************************
       !   Step 3 - double/double_rot do not agree with quad to
@@ -545,6 +561,7 @@ contains
          ! if(icheck.eq.1) write(*,*) 'passed quad vs quad_rot', amp(2), amprot(2), amp_qp(2), amprot_qp(2), prec 
          ! if(icheck.eq.3) write(*,*) 'failed quad vs quad_rot', amp(2), amprot(2), amp_qp(2), amprot_qp(2), prec 
       endif
+
       reduction_interoperation = tmp_red_int
    end subroutine rescue_qp
    !---#] subroutine rescue_qp[%
@@ -584,7 +601,8 @@ contains
          call rotation_check(vecs, scale2, amp, ampresrot, prec, icheck, ok, h)
       endif
       ! if(icheck.eq.1) write(*,*) 'passed rescue', amp(2), amprot(2), ampres(2), ampresrot(2), prec 
-      ! if(icheck.eq.3) write(*,*) 'failed rescue', amp(2), amprot(2), ampres(2), ampresrot(2), prec 
+      ! if(icheck.eq.2) write(*,*) 'failed rescue rotation_check', amp(2), amprot(2), ampres(2), ampresrot(2), prec 
+      ! if(icheck.eq.3) write(*,*) 'failed rescue pole_check', amp(2), amprot(2), ampres(2), ampresrot(2), prec 
       reduction_interoperation = reduction_interoperation_rescue
    end subroutine rescue
    !---#] subroutine rescue[%
@@ -636,11 +654,11 @@ contains
             ! sets prec, icheck
             call pole_check(vecs, scale2, amp, irp, prec, icheck, ok, h)
          endif
-         if(icheck.eq.2.or.(icheck.eq.3.and.PSP_rescue)) then
+         if(icheck.eq.2) then
             ! sets amprot, prec, icheck
             call rotation_check(vecs, scale2, amp, amprot, prec, icheck, ok, h)
          endif
-         if(icheck.eq.3.and.PSP_rescue) then[%
+         if(icheck.ne.1.and.PSP_rescue) then[%
          @if extension quadruple %]
             ! sets amp, prec, icheck
             call rescue_qp(vecs, scale2, amp, ampdef, amprot, ampres, ampresrot, prec, icheck, ok, h)[%
@@ -650,8 +668,9 @@ contains
          @end @if extension quadruple %]
          endif
 
-         if(icheck.eq.3) then
+         if(icheck.ne.1) then
             ! RESCUE SYSTEM HAS FAILED
+            icheck=3                                                    ! FAIL
             fpprec2=-10        ! Set -10 as finite part precision       ! DISCARD
             prec=min(prec,fpprec2)
          endif
