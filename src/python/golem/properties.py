gudrunhe/gosam

commit 573a02047912a4ef64ba2f26c505373eb33dabf2

One last optimization related to vanishing masses and width: make sure they are contained in 'zero' list for code generation. The issue was: in the built-in models the light quarks are defined with a mass parameter (e.g. mD for down quark), which would be set to zero in the parameters dict of the model. But GoSam would not recognize this and generate amplitudes with massive light quarks, unless the user manually included their masses in 'zero' (which was the case in all examples).

src/python/golem/util/main_misc.py
@@ -881,16 +881,43 @@ def workflow(conf):
             searchm = "mass("+str(abs(p.getPDGCode()))+")"
             if searchm in list(map(str.lower,new_zero)):
                 new_zero.pop(list(map(str.lower,new_zero)).index(searchm))
-                m = p.getMass()
-                if m != "0":
-                    new_zero.append(m)
+                if p.isMassive():
+                    new_zero.append(p.getMass())
             searchw = "width("+str(abs(p.getPDGCode()))+")"
             if searchw in list(map(str.lower,new_zero)):
                 new_zero.pop(list(map(str.lower,new_zero)).index(searchw))
-                w = p.getWidth()
-                if w != "0":
-                    new_zero.append(w)
+                if p.hasWidth():
+                    new_zero.append(p.getWidth())
         conf.setProperty("zero",",".join(list(set(new_zero))))
+
+
+    # It can happen that a model defines names for a particle's mass and width but 
+    # sets them to 0 in the parameters definiton (see e.g. the light quarks in the 
+    # built-in models). We have to take care of that and add those names to the zero 
+    # property to avoid erroneous code generation. Otherwise the user has to remember
+    # to add these cases to 'zero' manually.
+    # (can be skipped in OLP mode: already checked in util/olp.py:process_order_file)
+    if not conf["__OLP_MODE__"]:
+        zeros = conf.getListProperty("zero")
+        for p in model.particles.values():
+            if p.isMassive(zeros):
+                m = p.getMass(zeros)
+                try:
+                    if float(model.parameters[m]) == 0.:
+                        zeros.append(m)
+                except KeyError:
+                    # dependent parameters are not part of parameters dict
+                    pass
+            if p.hasWidth(zeros):
+                w = p.getWidth(zeros)
+                try:
+                    if float(model.parameters[w]) == 0.:
+                        zeros.append(w)
+                except KeyError:
+                    # dependent parameters are not part of parameters dict
+                    pass
+        conf.setProperty("zero",",".join(list(set(zeros))))
+
 
     for prop in [golem.properties.zero, golem.properties.one]:
         golem.util.tools.expand_parameter_list(prop, conf)

src/python/golem/util/olp.py
@@ -791,19 +791,43 @@ def process_order_file(
             searchm = "mass("+str(abs(p.getPDGCode()))+")"
             if searchm in list(map(str.lower,new_zero)):
                 new_zero.pop(list(map(str.lower,new_zero)).index(searchm))
-                m = p.getMass()
-                if m != "0":
-                    new_zero.append(m)
+                if p.isMassive():
+                    new_zero.append(p.getMass())
             searchw = "width("+str(abs(p.getPDGCode()))+")"
             if searchw in list(map(str.lower,new_zero)):
                 new_zero.pop(list(map(str.lower,new_zero)).index(searchw))
-                w = p.getWidth()
-                if w != "0":
-                    new_zero.append(w)
+                if p.hasWidth():
+                    new_zero.append(p.getWidth())
         conf.setProperty("zero",",".join(list(set(new_zero))))
         for subconf in subprocesses_conf:
             subconf.setProperty("zero",",".join(list(set(new_zero))))
 
+        # It can happen that a model defines names for a particle's mass and width but 
+        # sets them to 0 in the parameters definiton (see e.g. the light quarks in the 
+        # built-in models). We have to take care of that and add those names to the zero 
+        # property to avoid erroneous code generation. Otherwise the user has to remember
+        # to add these cases to 'zero' manually.
+        zeros = conf.getListProperty("zero")
+        for p in model.particles.values():
+            if p.isMassive(zeros):
+                m = p.getMass(zeros)
+                try:
+                    if float(model.parameters[m]) == 0.:
+                        zeros.append(m)
+                except KeyError:
+                    # dependent parameters are not part of parameters dict
+                    pass
+            if p.hasWidth(zeros):
+                w = p.getWidth(zeros)
+                try:
+                    if float(model.parameters[w]) == 0.:
+                        zeros.append(w)
+                except KeyError:
+                    # dependent parameters are not part of parameters dict
+                    pass
+        conf.setProperty("zero",",".join(list(set(zeros))))
+        for subconf in subprocesses_conf:
+            subconf.setProperty("zero",",".join(list(set(zeros))))
 
         # ---#[ Setup excluded and massive particles :
         for lconf in [conf] + subprocesses_conf:

commit cc49d3d834fb7d659b10cb7c748d65007abd413b

Adding a new property 'massive_light_fermions' allowing to set non-zero masses for fermions initialised with zero mass parameter in built-in model files. Necessary as 573a0204 broke the gggz example, which uses mU != 0.
I still believe that the current treatment of massless fermions is better, as the user is not required to manually adjust the 'zero' property to us use the built-in models in their default configuration. Instead non-default setups require manual adjustments.

doc/refman_sections/template.tex
@@ -180,6 +180,25 @@ See also: model, zero
 Default: \verb|None|
 \\Hidden: \verb|False|
 \\Experimental: \verb|False|
+\\\item[\colorbox{gray!30}{\texttt{massive\_light\_fermions}}] (\textit{true/false})
+\begin{verbatim}
+Some models define mass parameters for light fermions, but initialise
+them to zero. This is for example the case for the up- and down-quark,
+and the leptons in the built-in model files. Per default GoSam will
+treat those fermions as masseless during the code generation. The
+corresponding mass parameter in config.f90 is then fixed to zero and
+connot be changed. Setting
+
+massive_light_fermions=true
+
+deactivates this behaviour. Note that in this case the user can still
+add fermion mass parameters to the 'zero' property manually. For all
+other fermions a non-zero mass MUST be set when calling the process
+libraries.
+\end{verbatim}
+Default: \verb|False|
+\\Hidden: \verb|False|
+\\Experimental: \verb|False|
 \\\item[\colorbox{gray!30}{\texttt{nlo\_prefactors}}] (\textit{integer number})
 \begin{verbatim}
 

examples/gggz/gggz.rc
@@ -16,6 +16,8 @@ filter.nlo=lambda d: d.chord(["U", "D"]) > 2
 
 one=gs,e
 zero=w*, mD
+# example uses non-zero up-quark mass
+massive_light_fermions=true
 
 helicities=+-++,+-+-,+-+0
 

src/python/golem/properties.py
@@ -1622,6 +1622,27 @@ unitary_gauge = Property(
     default=False,
 )
 
+massive_light_fermions = Property(
+    "massive_light_fermions",
+    """\
+    Some models define mass parameters for light fermions, but initialise 
+    them to zero. This is for example the case for the up- and down-quark,
+    and the leptons in the built-in model files. Per default GoSam will 
+    treat those fermions as masseless during the code generation. The 
+    corresponding mass parameter in config.f90 is then fixed to zero and 
+    connot be changed. Setting 
+
+    massive_light_fermions=true
+
+    deactivates this behaviour. Note that in this case the user can still
+    add fermion mass parameters to the 'zero' property manually. For all
+    other fermions a non-zero mass MUST be set when calling the process 
+    libraries.
+    """,
+    bool,
+    default=False,
+)
+
 # Note: the order in the properties list determines the order of entries in the appendix of refman
 properties = [
 #   main process definition
@@ -1635,6 +1656,7 @@ properties = [
     loop_suppressed_Born,
     zero,
     one,
+    massive_light_fermions,
     config_nlo_prefactors,
     form_factor_lo,
     form_factor_nlo,

src/python/golem/util/main_misc.py
@@ -906,7 +906,7 @@ def workflow(conf):
     # property to avoid erroneous code generation. Otherwise the user has to remember
     # to add these cases to 'zero' manually.
     # (can be skipped in OLP mode: already checked in util/olp.py:process_order_file)
-    if not conf["__OLP_MODE__"]:
+    if not conf["__OLP_MODE__"] and not conf.getBooleanProperty("massive_light_fermions"):
         zeros = conf.getListProperty("zero")
         for p in model.particles.values():
             if p.isMassive(zeros):

src/python/golem/util/olp.py
@@ -807,24 +807,25 @@ def process_order_file(
         # built-in models). We have to take care of that and add those names to the zero 
         # property to avoid erroneous code generation. Otherwise the user has to remember
         # to add these cases to 'zero' manually.
-        zeros = conf.getListProperty("zero")
-        for p in model.particles.values():
-            if p.isMassive(zeros):
-                m = p.getMass(zeros)
-                try:
-                    if float(model.parameters[m]) == 0.:
-                        zeros.append(m)
-                except KeyError:
-                    # dependent parameters are not part of parameters dict
-                    pass
-            if p.hasWidth(zeros):
-                w = p.getWidth(zeros)
-                try:
-                    if float(model.parameters[w]) == 0.:
-                        zeros.append(w)
-                except KeyError:
-                    # dependent parameters are not part of parameters dict
-                    pass
+        if not conf.getBooleanProperty("massive_light_fermions"):
+            zeros = conf.getListProperty("zero")
+            for p in model.particles.values():
+                if p.isMassive(zeros):
+                    m = p.getMass(zeros)
+                    try:
+                        if float(model.parameters[m]) == 0.:
+                            zeros.append(m)
+                    except KeyError:
+                        # dependent parameters are not part of parameters dict
+                        pass
+                if p.hasWidth(zeros):
+                    w = p.getWidth(zeros)
+                    try:
+                        if float(model.parameters[w]) == 0.:
+                            zeros.append(w)
+                    except KeyError:
+                        # dependent parameters are not part of parameters dict
+                        pass
         conf.setProperty("zero",",".join(list(set(zeros))))
         for subconf in subprocesses_conf:
             subconf.setProperty("zero",",".join(list(set(zeros))))